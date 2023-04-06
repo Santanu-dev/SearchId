fd86874a1d8d0cda4dcfb62517b10eaf08b0b664
Santanu Barman <barmansantanu11@gmail.com>
2023-04-06 20:26:09 +0530

    combined two components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Search from './component/Search';
 import Todo from './component/Todo';
+import React, { useState } from 'react';
 
 function App() {
 
+  const [selected, setSelected] = useState('search')
+  const handleChange = (e) => {
+    setSelected(e.target.value)
+  }
   return (
     <>
-      <Search />
-      {/* <Todo /> */}
+    <select onChange={handleChange} style={{marginLeft: "45%", marginTop: '10px', marginBottom: '10px'}}>
+      <option value="search">Search</option>
+      <option value="todo">Todod List</option>
+    </select>
+    { selected === 'search' ? 
+      <Search /> : 
+      <Todo /> }
     </>
   );
 }
